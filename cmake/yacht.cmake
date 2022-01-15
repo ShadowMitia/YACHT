f9914cd075ebea31f22ca5590dabe8fe50cbc4d9
Dimitri Belopopsky <dimitri@belopopsky.com>
2022-01-15 16:06:55 +0100

    Make YACHT be self contained in its own folder

---
+++ cmake/yacht.cmake
@@ -0,0 +1,6 @@
+# YACHT: Yet Another Cpp Helper Template
+## A template for C++ projects.
+## More info : https://github.com/ShadowMitia/YACHT/
+
+include(${PROJECT_SOURCE_DIR}/cmake/YACHT/load_config.cmake)
+include(${PROJECT_SOURCE_DIR}/cmake/YACHT/load_tools.cmake)
